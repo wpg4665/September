30c0af737071e85a1c6864704becd3eefca37752
Will Gordon <wpg4665@gmail.com>
2014-09-20 16:03:09 -0400

    Other pages created Pics added Ceremony info added

--- Wedding/urls.py
+++ Wedding/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('Wedding.views',
-	url(r'^$', 'index', name='index'),
-	url(r'^contact/', 'contact', name='contact'),
+	url(r'^(|ceremony|reception|guest-info|wedding-party)$', 'main_pages'),
 	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/Wedding/favicon.ico')),
 )
--- Wedding/views.py
+++ Wedding/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render_to_response
 from datetime import datetime
 
 
-def index(request):
+def main_pages(request, curr_url):
+	d, f = calc_date_left()
+	curr_url = 'index' if curr_url == '' else curr_url
+	return render_to_response('Wedding/' + str(curr_url) + '.html', {'diff': d, 'wedd_date': f})
+
+
+def calc_date_left():
 	wedding_date = datetime(2015, 9, 4, 16, 30, 0)
 	ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(n / 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
 	delta = wedding_date - datetime.now()
@@ -16,8 +22,5 @@ def index(request):
 	formatted_date["day"] = "The " + ordinal(wedding_date.day)
 	formatted_date["month"] = "of " + wedding_date.strftime("%b")
 	formatted_date["year"] = wedding_date.strftime("%Y")
-	return render_to_response('Wedding/index.html', {'diff': difference, 'wedd_date': formatted_date})
 
-
-def contact():
-	pass
+	return difference, formatted_date
