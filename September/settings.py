2d79a96813d51914477833e4271456143121bd89
Will Gordon <wpg4665@gmail.com>
2014-09-27 14:32:49 -0400

    Damn 320px widths! =/

--- September/settings.py
+++ September/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Wedding',
-    'adminsortable',
 )
 
 MIDDLEWARE_CLASSES = (
--- Wedding/admin.py
+++ Wedding/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 from Wedding.models import Article
-from adminsortable.admin import SortableAdmin
 
-
-class ArticleAdmin(SortableAdmin, admin.ModelAdmin):
-	pass
-
-admin.site.register(Article, ArticleAdmin)
