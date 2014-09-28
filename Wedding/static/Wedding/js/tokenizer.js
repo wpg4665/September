2ec74a1849ea0b907cc0305a2e365f14bcf2e897
Will Gordon <wpg4665@gmail.com>
2014-09-28 00:33:34 -0400

    Finished with design...and so very tired =/ Ready for Reddit critique!

--- Wedding/static/Wedding/js/tokenizer.js
+++ Wedding/static/Wedding/js/tokenizer.js
@@ -1,3 +1,38 @@
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun/*, thisArg*/) {
+    'use strict';
+
+    if (this === void 0 || this === null) {
+      throw new TypeError();
+    }
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+
+        // NOTE: Technically this should Object.defineProperty at
+        //       the next index, as push can be affected by
+        //       properties on Object.prototype and Array.prototype.
+        //       But that method's new, and collisions should be
+        //       rare, so use the more-compatible alternative.
+        if (fun.call(thisArg, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+}
+
 (function (root, factory) {
     // Universal Module Definition (UMD) to support AMD, CommonJS/Node.js,
     // Rhino, and plain browser loading.
@@ -284,7 +319,7 @@ function tokenize(str, options) {
 			else if(code == 0x28) {
 				if(currtoken.ASCIImatch('url')) switchto('url');
 				else emit(new FunctionToken(currtoken)) && switchto('data');
-			} 
+			}
 			else if(whitespace(code) && options.transformFunctionWhitespace) switchto('transform-function-whitespace') && reconsume();
 			else emit() && switchto('data') && reconsume();
 			break;
