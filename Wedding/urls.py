3bb84174a300bc0452bbc92e6dc6f220f3e128cf
Will Gordon <wpg4665@gmail.com>
2015-04-28 19:23:09 -0400

    Better static handling

--- Wedding/urls.py
+++ Wedding/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf import settings
+from django.conf.urls import patterns, url, static
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('Wedding.views',
 	url(r'^(|ceremony|reception|guest-info|wedding-party)$', 'main_pages'),
 	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/Wedding/favicon.ico')),
 )
+
+if settings.DEBUG:
+    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
